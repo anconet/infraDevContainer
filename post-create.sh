5a538853d5b8d502997632242ca75e790e474860
Anthony Anconetani <anthony.anconetani@gmail.com>
2026-04-19 04:51:10 -0500

    Add .gitignore, update devcontainer.json, and modify post-create.sh for environment setup

--- post-create.sh
+++ post-create.sh
@@ -15,44 +15,14 @@ DEBIAN_FRONTEND=noninteractive sudo apt install -y --no-install-recommends \
     curl \
     dbus-x11 \
     git \
-    gtkwave \
-    iverilog \
-    jq \
     python3-pip \
-    universal-ctags \
-    verilator \
     wget \
     make \
     vim \
     less
-pip3 install \
-    cocotb \
-    cocotb-test \
-    flake8 \
-    isort \
-    pytest \
-    yapf
- 
-# Verible
-ARCH=$(uname -m)
-if [[ $ARCH == "aarch64" ]]; then
-    ARCH="arm64"
-fi
-DIST_ID=$(grep DISTRIB_ID /etc/lsb-release | cut -d'=' -f2)
-DIST_RELEASE=$(grep RELEASE /etc/lsb-release | cut -d'=' -f2)
-DIST_CODENAME=$(grep CODENAME /etc/lsb-release | cut -d'=' -f2)
-VERIBLE_RELEASE=$(curl -s -X GET https://api.github.com/repos/chipsalliance/verible/releases/latest | jq -r '.tag_name')
-VERIBLE_TAR=verible-$VERIBLE_RELEASE-linux-static-$ARCH.tar.gz
-if [[ ! -f $VERIBLE_TAR ]]; then
-    wget https://github.com/chipsalliance/verible/releases/download/$VERIBLE_RELEASE/$VERIBLE_TAR
-fi
-if [[ ! -f "/usr/local/bin/verible-verilog-format" ]]; then
-    sudo tar -C /usr/local --strip-components 1 -xf $VERIBLE_TAR
-fi
-rm $VERIBLE_TAR
 
 # Persist alias for future interactive shells.
-BUILD_ALIAS="alias build='python3 /workspaces/infraVerilogSetupTest/build.py'"
-if ! grep -Fxq "$BUILD_ALIAS" "$HOME/.bashrc"; then
-    echo "$BUILD_ALIAS" >> "$HOME/.bashrc"
-fi
+#BUILD_ALIAS="alias build='python3 /workspaces/infraVerilogSetupTest/build.py'"
+#if ! grep -Fxq "$BUILD_ALIAS" "$HOME/.bashrc"; then
+#    echo "$BUILD_ALIAS" >> "$HOME/.bashrc"
+#fi
